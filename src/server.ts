c805595b9a351e4701b9beb18d44d831dd28a0fb
zhaolong <naokimidori@163.com>
2021-05-21 23:18:07 +0800

    feat: initial

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import Koa from 'koa'
+import cors from '@koa/cors'
+import bodyParser from 'koa-bodyparser'
+
+
+const app = new Koa()
+
+app.use(cors())
+app.use(bodyParser())
+
+app.use((ctx) => {
+  ctx.body = 'hello vino~'
+})
+
+app.listen(3000)
