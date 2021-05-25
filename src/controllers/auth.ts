feb81fa713de55cf0db59bb95c7ae9488e39a634
zhaolong <naokimidori@163.com>
2021-05-25 23:08:55 +0800

    feat: auth argon2

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,23 @@
-import { Context } from 'koa'
+import { Context } from "koa";
+import * as argon2 from "argon2";
+import { getManager } from "typeorm";
 
+import { User } from "../entity/user";
 export default class AuthController {
   public static async login(ctx: Context) {
-    ctx.body = 'Login Controller'
+    ctx.body = "Login Controller";
   }
-  
+
   public static async register(ctx: Context) {
-    ctx.body = 'Register Controller'
+    const userRepository = getManager().getRepository(User);
+    const newUer = new User();
+    newUer.name = ctx.request.body.name;
+    newUer.email = ctx.request.body.email;
+    newUer.password = await argon2.hash(ctx.request.body.password);
+
+    const user = await userRepository.save(newUer);
+
+    ctx.status = 201;
+    ctx.body = user;
   }
 }
