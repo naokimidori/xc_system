cfa44cf53f0246726e6ee9e2eb962e03c9ad2984
zhaolong <naokimidori@163.com>
2021-05-25 23:31:41 +0800

    feat: JWT routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,16 +3,18 @@ import Router from '@koa/router'
 import AuthController from './controllers/auth'
 import UserController from './controllers/user'
 
-const router = new Router()
+const unprotectedRouter = new Router()
 
 // auth相关的路由
-router.post('/auth/login', AuthController.login)
-router.post('/auth/register', AuthController.register)
+unprotectedRouter.post('/auth/login', AuthController.login)
+unprotectedRouter.post('/auth/register', AuthController.register)
+
+const protectedRouter = new Router()
 
 // user相关的路由
-router.get('/users', UserController.allUsers)
-router.get('/users/:id', UserController.showUserDetail)
-router.put('/users/:id', UserController.updateUser)
-router.delete('/user/:id', UserController.deleteUser)
+protectedRouter.get('/users', UserController.allUsers)
+protectedRouter.get('/users/:id', UserController.showUserDetail)
+protectedRouter.put('/users/:id', UserController.updateUser)
+protectedRouter.delete('/user/:id', UserController.deleteUser)
 
-export default router
+export { unprotectedRouter, protectedRouter }
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import Koa from 'koa'
 import cors from '@koa/cors'
 import bodyParser from 'koa-bodyparser'
 import { createConnection } from 'typeorm'
+import jwt from 'koa-jwt'
 
 import { logger } from './logger'
-import router from './routes'
+import { unprotectedRouter, protectedRouter } from './routes'
+import { JWT_SECRET } from './constant'
 
 createConnection()
   .then(() => {
@@ -15,8 +17,14 @@ createConnection()
     app.use(cors())
     app.use(bodyParser())
 
-    // 响应用户请求
-    app.use(router.routes()).use(router.allowedMethods())
+    // 无需JWT即可访问
+    app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods())
+
+    // 注册JWT中间件
+    app.use(jwt({ secret: JWT_SECRET }).unless({ method: 'GET' }))
+
+    // 需JWT才能访问
+    app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods())
 
     app.listen(3000)
   })
